ff2e18f4db9c794bf9fa8a2014b5aa8c8f913836
Ivan <ivan.frontcoder@gmail.com>
2022-08-10 22:03:09 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,95 @@
 window.onload = function () {
 
-  // Swiper
-  if ($('#swiper').length) {
-    const swiper = new Swiper('#swiper', {
-      // Optional parameters
-      slidesPerView: 1,
+  // // Swiper
+  // if ($('#swiper').length) {
+  //   const swiper = new Swiper('#swiper', {
+  //     // Optional parameters
+  //     slidesPerView: 1,
 
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+  //     // If we need pagination
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       clickable: true,
+  //     },
 
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+  //     // Navigation arrows
+  //     navigation: {
+  //       nextEl: '.swiper-button-next',
+  //       prevEl: '.swiper-button-prev',
+  //     },
 
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
-  }
+  //     // And if we need scrollbar
+  //     scrollbar: {
+  //       el: '.swiper-scrollbar',
+  //     },
+  //   });
+  // }
 
-  // Air Datepicker
-  new AirDatepicker('#airDatepicker', {
-    position: 'right top',
-  });
+  // // Air Datepicker
+  // new AirDatepicker('#airDatepicker', {
+  //   position: 'right top',
+  // });
 
-  // Magnific Popup
-  $('.open-popup-link').magnificPopup({
-    mainClass: 'mfp-fade'
-  });
+  // // Magnific Popup
+  // $('.open-popup-link').magnificPopup({
+  //   mainClass: 'mfp-fade'
+  // });
 
-  // Табы
-	function tabs() {
-    const tabs = $('.js-tabs');
-		if (tabs.length) {
-			tabs.each( function () {
-        let triggers = $(this).find('.js-tabs-trigger');
-        let contents = $(this).find('.js-tabs-content');
-        let time = 300;
-        triggers.on('click', function () {
-          let trigger = $(this);
-          let content = $('.js-tabs-content[data-href="' + trigger.attr('href') +'"]');
-          if (!trigger.hasClass('active')) {
-            triggers.removeClass('active');
-            trigger.addClass('active');
-            contents.hide();
-            contents.removeClass('open');
-            content.fadeIn(time, function () {
-              $(this).addClass('open');
-            });
-          }else {
-            return false;
-          }
-        })
-      });
-		}
+  // // Табы
+	// function tabs() {
+  //   const tabs = $('.js-tabs');
+	// 	if (tabs.length) {
+	// 		tabs.each( function () {
+  //       let triggers = $(this).find('.js-tabs-trigger');
+  //       let contents = $(this).find('.js-tabs-content');
+  //       let time = 300;
+  //       triggers.on('click', function () {
+  //         let trigger = $(this);
+  //         let content = $('.js-tabs-content[data-href="' + trigger.attr('href') +'"]');
+  //         if (!trigger.hasClass('active')) {
+  //           triggers.removeClass('active');
+  //           trigger.addClass('active');
+  //           contents.hide();
+  //           contents.removeClass('open');
+  //           content.fadeIn(time, function () {
+  //             $(this).addClass('open');
+  //           });
+  //         }else {
+  //           return false;
+  //         }
+  //       })
+  //     });
+	// 	}
+	// }
+	// tabs();
+
+  // Выпадайки при клике по кнопке
+	// Задать блокам выпадайкам айдишник совпадающий с data-drop="" в кнопке для этого блока
+	// Задать кнопкам .js-drop-btn и data-drop="" с айдишником блока выпадайки
+	function dropBlock(btn) {
+		var $this = undefined,
+				drop = undefined,
+				close = $('.js-drop-close');
+		btn.on('click', function () {
+			$this = $(this);
+			drop = $('#' + $this.data('drop'));
+			$this.toggleClass('is-active');
+			drop.toggleClass('open');
+			$(document).mouseup(function (e) {
+				if (!$this.is(e.target)
+					&& $this.has(e.target).length === 0
+					&& !drop.is(e.target)
+					&& drop.has(e.target).length === 0) {
+					$this.removeClass('is-active');
+					drop.removeClass('open');
+				}
+			});
+		})
+		close.on('click', function () {
+			$('[data-drop="' + $(this).data('drop') +'"]').removeClass('is-active');
+			$('#' + $(this).data('drop')).removeClass('open');
+		})
 	}
-	tabs();
+	dropBlock($('.js-drop-btn'));
 
 }
