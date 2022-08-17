23b7f01d2efbe35389f8b658a252ba5ba68b688c
Ivan <ivan.frontcoder@gmail.com>
2022-08-17 12:37:39 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,20 @@
 window.onload = function () {
 
-  // // Swiper
-  // if ($('#swiper').length) {
-  //   const swiper = new Swiper('#swiper', {
-  //     // Optional parameters
-  //     slidesPerView: 1,
-
-  //     // If we need pagination
-  //     pagination: {
-  //       el: '.swiper-pagination',
-  //       clickable: true,
-  //     },
-
-  //     // Navigation arrows
-  //     navigation: {
-  //       nextEl: '.swiper-button-next',
-  //       prevEl: '.swiper-button-prev',
-  //     },
-
-  //     // And if we need scrollbar
-  //     scrollbar: {
-  //       el: '.swiper-scrollbar',
-  //     },
-  //   });
-  // }
-
-  // // Air Datepicker
-  // new AirDatepicker('#airDatepicker', {
-  //   position: 'right top',
-  // });
-
-  // // Magnific Popup
-  // $('.open-popup-link').magnificPopup({
-  //   mainClass: 'mfp-fade'
-  // });
-
-  // // Табы
-	// function tabs() {
-  //   const tabs = $('.js-tabs');
-	// 	if (tabs.length) {
-	// 		tabs.each( function () {
-  //       let triggers = $(this).find('.js-tabs-trigger');
-  //       let contents = $(this).find('.js-tabs-content');
-  //       let time = 300;
-  //       triggers.on('click', function () {
-  //         let trigger = $(this);
-  //         let content = $('.js-tabs-content[data-href="' + trigger.attr('href') +'"]');
-  //         if (!trigger.hasClass('active')) {
-  //           triggers.removeClass('active');
-  //           trigger.addClass('active');
-  //           contents.hide();
-  //           contents.removeClass('open');
-  //           content.fadeIn(time, function () {
-  //             $(this).addClass('open');
-  //           });
-  //         }else {
-  //           return false;
-  //         }
-  //       })
-  //     });
-	// 	}
-	// }
-	// tabs();
+  // Stiky menu // Липкое меню.
+	function stikyMenu(header) {
+		headerTop = header.offset().top;
+		$(window).scroll(function(){
+      headerHoggleClass();
+		});
+    function headerHoggleClass() {
+      if ($(window).scrollTop() > headerTop + 150) {
+        header.addClass('stiky');
+      } else {
+        header.removeClass('stiky');
+      }
+    }
+	};
+	stikyMenu($('#headerSticky'));
 
   // Выпадайки при клике по кнопке
 	// Задать блокам выпадайкам айдишник совпадающий с data-drop="" в кнопке для этого блока
@@ -91,5 +44,28 @@ window.onload = function () {
 		})
 	}
 	dropBlock($('.js-drop-btn'));
+
+  // Кнопка скролла страницы наверх
+  function scrollUp() {
+    const btn = $('.scrollup');
+    $(window).scroll(function () {
+      btnShowFade();
+    });
+    function btnShowFade() {
+      if ($(this).scrollTop() > 200) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    }
+    btnShowFade();
+    btn.click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 500);
+      return false;
+    });
+  }
+  scrollUp();
 
 }
